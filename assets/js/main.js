751b6afd35c5304dbb28877eff958712b4b5e522
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-05-10 23:38:55 +0530

    portfolio project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,22 +70,16 @@ tabs.forEach(tab => {
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper('.portfolio__container', {
     cssMode: true,
-    loop:true,
+    loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
     pagination: {
         el: '.swipper-pagination',
-        clickable:true,
+        clickable: true,
     },
 })
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
@@ -93,4 +87,29 @@ let swiper = new Swiper('.portfolio__container', {
 /*==================== SHOW SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
+
+const themeButton = document.getElementById('theme-button')
+const darktheme = 'dark-theme'
+const icontheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getcurrentTheme = () => document.body.classList.contains(darktheme) ? "dark" : "light"
+const getcurrenticon = () => themeButton.classList.contains(icontheme) ? 'uil-moon' : 'uil-sun'
+
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darktheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](icontheme)
+}
+// Activate/deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darktheme)
+    themeButton.classList.toggle(icontheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
